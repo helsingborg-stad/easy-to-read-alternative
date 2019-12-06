cf8de02081c7f02dce3b5db3b1f04c6a8011b11e
Ehsan Zilaei <ehsan.zilaei@helsingborg.se>
2019-12-06 15:26:41 +0100

    Fix for when tag more used without more content.

--- source/php/Posts/Content.php
+++ source/php/Posts/Content.php
@@ -4,76 +4,91 @@ namespace EasyReading\Posts;
 
 class Content
 {
-	public function __construct()
-	{
-		$theme = wp_get_theme();
-		if ('Municipio' == $theme->name || 'Municipio' == $theme->parent_theme) {
-			add_filter('accessibility_items', array($this, 'addAccessibility'), 11);
-		}
-		add_filter('the_lead', array($this, 'easyReadingLead'), 11);
-		add_filter('the_content', array($this, 'easyReadingContent'), 10);
-	}
+    public function __construct()
+    {
+        $theme = wp_get_theme();
+        if ('Municipio' == $theme->name || 'Municipio' == $theme->parent_theme) {
+            add_filter('accessibility_items', array($this, 'addAccessibility'), 11);
+        }
+        add_filter('the_lead', array($this, 'easyReadingLead'), 11);
+        add_filter('the_content', array($this, 'easyReadingContent'), 10);
+    }
 
-	/**
-	 * Add easy to read link to accessibility nav
-	 * @param  array $items Default items
-	 * @return array       	Modified items
-	 */
-	public function addAccessibility($items): array
-	{
-		global $wp;
-		$current_url = home_url(add_query_arg(array(),$wp->request));
+    /**
+     * Add easy to read link to accessibility nav
+     * @param  array $items Default items
+     * @return array       	Modified items
+     */
+    public function addAccessibility($items): array
+    {
+        global $wp;
+        $current_url = home_url(add_query_arg(array(),$wp->request));
 
-		if (! isset($_GET['readable']) && get_field('easy_reading_select') == true) {
-			$items[] = '<a href="' . add_query_arg('readable', '1', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Easy to read', 'easy-reading') . '</a>';
-    	} elseif(isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true) {
-    		$items[] = '<a href="' . remove_query_arg('readable', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Default version', 'easy-reading') . '</a>';
-    	}
+        if (! isset($_GET['readable']) && get_field('easy_reading_select') == true) {
+            $items[] = '<a href="' . add_query_arg('readable', '1', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Easy to read', 'easy-reading') . '</a>';
+        } elseif(isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true) {
+            $items[] = '<a href="' . remove_query_arg('readable', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Default version', 'easy-reading') . '</a>';
+        }
 
-    	return $items;
-	}
+        return $items;
+    }
 
-	/**
-	 * Remove the lead
-	 * @param  string $lead Default lead
-	 * @return string       Modified lead
-	 */
-	public function easyReadingLead($lead)
-	{
-		if (isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true && in_the_loop() && is_main_query()) {
-			return '';
-		}
+    /**
+     * Remove the lead
+     * @param  string $lead Default lead
+     * @return string       Modified lead
+     */
+    public function easyReadingLead($lead)
+    {
+        global $post;
 
-		return $lead;
-	}
+        if($this->shouldDisplay($post)) {
+            return "";
+        }
 
-	/**
-	 * Switch content to alternate version
-	 * @param  string $content Default content
-	 * @return string       Modified content
-	 */
-	public function easyReadingContent($content)
-	{
-		global $post;
+        return $lead;
+    }
 
-		if (isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true && is_object($post) && isset($post->post_content) && in_the_loop() && is_main_query()) {
-			$post_content = $post->post_content;
-			if (strpos($post_content,  '<!--more-->') !== false) {
-				$content_parts = explode('<!--more-->', $post_content);
-				$post_content  = $content_parts[1];
-			}
-			$post_content 	   = preg_replace('/[^a-z]/i', '', sanitize_text_field($post_content));
-			$sanitized_content = preg_replace('/[^a-z]/i', '', sanitize_text_field($content));
+    /**
+     * Switch content to alternate version
+     * @param  string $content Default content
+     * @return string       Modified content
+     */
+    public function easyReadingContent($content)
+    {
+        global $post;
 
-			if ($post_content == $sanitized_content) {
-				$content = get_field('easy_reading_content');
-				if (strpos($content,  '<!--more-->') !== false) {
-					$content_parts = explode('<!--more-->', $content);
-					$content = '<p class="lead">' . sanitize_text_field($content_parts[0]) . '</p>' . $content_parts[1];
-				}
-			}
-		}
+        if($this->shouldDisplay($post)) {
+            remove_filter('the_content', array($this, 'easyReadingContent'));
+            $content = apply_filters('the_content', get_field('easy_reading_content'));
+            add_filter('the_content', array($this, 'easyReadingContent'), 10);
+        }
 
-		return $content;
-	}
+        return $content;
+    }
+
+    /**
+     * Detect if alternate content should be delivered
+     * @param  string $post The post object
+     * @return bool   If the easy read text should be displayed
+     */
+    public function shouldDisplay($post) {
+        if(!(isset($_GET['readable']) && $_GET['readable'] == '1')) {
+            return false;
+        }
+
+        if(!(is_object($post) && isset($post->post_content))) {
+            return false;
+        }
+
+        if(!(in_the_loop() && is_main_query())) {
+            return false;
+        }
+
+        if(get_field('easy_reading_select') == false) {
+            return false;
+        }
+
+        return true;
+    }
 }
