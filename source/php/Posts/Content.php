323079edb1a7a62ccc65817897f33a6b1ab2f2b6
Dan Nilsson <dan.nilsson@helsingborg.se>
2019-12-11 17:25:20 +0100

    Refactor and fix bug content not showing when clicking on 'Lättläst'

--- source/php/Posts/Content.php
+++ source/php/Posts/Content.php
@@ -4,76 +4,115 @@ namespace EasyReading\Posts;
 
 class Content
 {
-	public function __construct()
-	{
-		$theme = wp_get_theme();
-		if ('Municipio' == $theme->name || 'Municipio' == $theme->parent_theme) {
-			add_filter('accessibility_items', array($this, 'addAccessibility'), 11);
-		}
-		add_filter('the_lead', array($this, 'easyReadingLead'), 11);
-		add_filter('the_content', array($this, 'easyReadingContent'), 10);
-	}
+    private $_isReadable = false;
 
-	/**
-	 * Add easy to read link to accessibility nav
-	 * @param  array $items Default items
-	 * @return array       	Modified items
-	 */
-	public function addAccessibility($items): array
-	{
-		global $wp;
-		$current_url = home_url(add_query_arg(array(),$wp->request));
+    public function __construct()
+    {
+        $this->setIsReadable();
+        $theme = wp_get_theme();
 
-		if (! isset($_GET['readable']) && get_field('easy_reading_select') == true) {
-			$items[] = '<a href="' . add_query_arg('readable', '1', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Easy to read', 'easy-reading') . '</a>';
-    	} elseif(isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true) {
-    		$items[] = '<a href="' . remove_query_arg('readable', $current_url) . '" class=""><i class="pricon pricon-easy-read"></i> ' . __('Default version', 'easy-reading') . '</a>';
-    	}
+        $themeName = 'Municipio';
 
-    	return $items;
-	}
+        if (
+            $themeName == $theme->name ||
+            $themeName == $theme->parent_theme
+        ) {
+            add_filter('accessibility_items', array($this, 'addAccessibility'), 10);
+        }
 
-	/**
-	 * Remove the lead
-	 * @param  string $lead Default lead
-	 * @return string       Modified lead
-	 */
-	public function easyReadingLead($lead)
-	{
-		if (isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true && in_the_loop() && is_main_query()) {
-			return '';
-		}
+        add_filter('the_lead', array($this, 'easyReadingLead'), 10);
+        add_filter('the_content', array($this, 'easyReadingContent'), 15);
+    }
 
-		return $lead;
-	}
+    /**
+     * Add easy to read link to accessibility nav
+     * @param  array $items Default items
+     * @return array       	Modified items
+     */
+    public function addAccessibility($items): array
+    {
+        $hasField = get_field('easy_reading_select');
 
-	/**
-	 * Switch content to alternate version
-	 * @param  string $content Default content
-	 * @return string       Modified content
-	 */
-	public function easyReadingContent($content)
-	{
-		global $post;
+        if ($hasField && !$this->getIsReadable()) {
+            $hrefUrl = add_query_arg('readable', true, get_permalink());
+            $linkText = __('Easy to read', 'easy-reading');
+        } elseif ($hasField && $this->getIsReadable()) {
+            $hrefUrl = remove_query_arg('readable', get_permalink());
+            $linkText = __('Default version', 'easy-reading');
+        }
 
-		if (isset($_GET['readable']) && $_GET['readable'] == '1' && get_field('easy_reading_select') == true && is_object($post) && isset($post->post_content) && in_the_loop() && is_main_query()) {
-			$post_content = $post->post_content;
-			if (strpos($post_content,  '<!--more-->') !== false) {
-				$content_parts = explode('<!--more-->', $post_content);
-				$post_content  = $content_parts[1];
-			}
-			$post_content 	   = preg_replace('/[^a-z]/i', '', sanitize_text_field($post_content));
-			$sanitized_content = preg_replace('/[^a-z]/i', '', sanitize_text_field($content));
+        $items[] = sprintf('
+            <a href="%s" class="">
+                <i class="pricon pricon-easy-read"></i>
+                %s
+            </a>',
+            $hrefUrl,
+            $linkText
+        );
 
-			if ($post_content == $sanitized_content) {
-				$content = get_field('easy_reading_content');
-				if (strpos($content,  '<!--more-->') !== false) {
-					$content_parts = explode('<!--more-->', $content);
-					$content = '<p class="lead">' . sanitize_text_field($content_parts[0]) . '</p>' . $content_parts[1];
-				}
-			}
-		}
+        return $items;
+    }
 
-		return $content;
-	}
+    /**
+     * Remove the lead
+     * @param  string $lead Default lead
+     * @return string       Modified lead
+     */
+    public function easyReadingLead($lead): string
+    {
+        if($this->_isShowable()) {
+            apply_filters('the_content', null);
+            return '';
+        }
+
+        return $lead;
+    }
+
+    /**
+     * Switch content to alternate version
+     * @param  string $content 	Default content
+     * @return string       	Modified content
+     */
+    public function easyReadingContent($content)
+    {
+        if($this->_isShowable()) {
+            $easyReadingContent = get_field('easy_reading_content');
+            return $easyReadingContent;
+        }
+
+        return $content;
+    }
+
+    /**
+     * Get isReadable property
+     * @return bool
+     */
+    public function getIsReadable(): bool
+    {
+        return (bool) $this->_isReadable;
+    }
+
+    /**
+     * Set isReadable property via query string param
+     * @return bool
+     */
+    public function setIsReadable(): void
+    {
+        $queryParam = substr($_GET['readable'], 0, 1);
+        $this->_isReadable = $queryParam === '1' ? true : false;
+    }
+
+    /**
+     * Check if easy reading content is to be showable
+     * @return bool
+     */
+    private function _isShowable(): bool
+    {
+        return (
+            $this->getIsReadable() &&
+            get_field('easy_reading_select') &&
+            in_the_loop() &&
+            is_main_query()
+        );
+    }
 }
